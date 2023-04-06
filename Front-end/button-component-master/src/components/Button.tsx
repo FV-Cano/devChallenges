8e31c20aa56a0fafa660dc050d9554da8b52aeb2
FV-Cano (Federico Vicente Cano) <cano.federicovicente@gmail.com>
2023-04-06 17:25:55 -0300

    Button prototype

---
+++ Front-end/button-component-master/src/components/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles/Button.css";
+
+interface ButtonProps {
+	variant: "text" | "outlined";
+	disableShadow: boolean;
+	disabled: boolean;
+	startIcon: string;
+	endIcon: string;
+	size: "sm" | "md" | "lg";
+	color: "default" | "primary" | "secondary" | "danger";
+	content: string;
+}
+
+export const Button = ({
+	variant,
+	disableShadow,
+	disabled,
+	startIcon,
+	endIcon,
+	size,
+	color,
+	content,
+}: ButtonProps) => {
+	return (
+		<button
+			className={`button ${variant} ${size} ${color}
+            ${disableShadow ? "" : "shadow"}
+            ${disabled ? "disabled" : ""}
+            `}
+		>
+			{/* prettier-ignore */}
+			{startIcon ? <span className="material-icons">{startIcon}</span> : ""}
+			{<span className="textContent">{content}</span>}
+			{endIcon ? <span className="material-icons">{endIcon}</span> : ""}
+		</button>
+	);
+};
